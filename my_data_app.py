cc95e11e0d053cd207f8715808a0fb50627e0c75
Igor BALLO <sodballo@gmail.com>
2025-07-17 14:39:42 +0000

    telechargement

--- my_data_app.py
+++ my_data_app.py
@@ -1,7 +1,11 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import requests
+from bs4 import BeautifulSoup
+import time
 import base64
+import io
 import plotly.express as px
 import plotly.graph_objects as go
 from datetime import datetime
